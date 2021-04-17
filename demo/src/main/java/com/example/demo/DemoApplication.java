dec47b4ecb741049ba864795cc3fe6c9effc259d
liyou <303181885@qq.com>
2021-04-17 16:11:20 +0800

    test

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    /*
+    * function:
+    * param:
+    * throw:
+    */
+    /*
+    * function:
+    * param:
+    * throw:
+    */
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/test.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class test {
+
+    String age;
+
+    public test(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "test{" +
+                "age='" + age + '\'' +
+                '}';
+    }
+}
